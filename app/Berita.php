c80ad55d28506e7f0edca79fba010eccb5e1e854
khairahmanplus <khairahmanplus@gmail.com>
2017-05-02 16:53:05 +0800

    Fixing polymorphic relations

--- app/Berita.php
+++ app/Berita.php
@@ -2,21 +2,41 @@
 
 namespace App;
 
-use App\User;
-use App\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Berita extends Model
 {
-    protected $fillable = ['tajuk', 'huraian', 'lokasi', 'kumpulan_sasaran', 'gambar'];
-    
-    protected $dates = ['created_at', 'updated_at', 'expired_at'];
+    /**
+     *
+     */
+    protected $fillable = [
+        'tajuk',
+        'huraian',
+        'lokasi',
+        'kumpulan_sasaran',
+        'gambar'
+    ];
 
+    /**
+     *
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'expired_at'
+    ];
+
+    /**
+     *
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     *
+     */
     public function categories()
     {
       return $this->morphMany(Category::class, 'categorizable');
--- app/Category.php
+++ app/Category.php
@@ -5,20 +5,20 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
 
-Relation::morphMap([
-	'beritas' => 'Berita::class',
-	'events' => 'Event::class',
-]);
-
 class Category extends Model
 {
+	/**
+	 *
+	 */
     protected $fillable = [
         'name'
     ];
 
+	/**
+	 *
+	 */
     public function categorizable()
     {
         return $this->morphTo();
     }
-
 }
